2d5520f8519aa22b6368459ad091170e4955fec2
Jessop Breth <jessopb@gmail.com>
2018-08-24 17:15:49 -0400

    Players play to their collections, Makes ~playable

--- rummy2.py
+++ rummy2.py
@@ -1,4 +1,3 @@
-import random
 
 # Setup
 players = 0
@@ -21,8 +20,8 @@ def setup_game():
 
     def shuffle(deck):
         for i in range(1000):
-            a = random.randrange(0, 51)
-            b = random.randrange(0, 51)
+            a = random.randint(0, 51)
+            b = random.randint(0, 51)
             deck[a], deck[b] = deck[b], deck[a]
         return deck
 
@@ -41,18 +40,10 @@ def setup_game():
     for p in range(players):
         hands.append([])
         collections.append([])
-        # test collections_to_str
-        # collections[p].append(['AH','AC','AD'])
         for c in range(7):
             hands[p].append(draw())
     # flip discard
     discard_pile.append(draw())
-    print("Discard pile:", discard_pile)
-    # for p in range(players):
-    #     print("Player",p)
-    #     for c in range(len(playerhands[p])):
-    #         print(playerhands[p][c])
-    print('Hands:', hands)
 
 
 def print_game_for_player(p):
@@ -62,12 +53,18 @@ def print_game_for_player(p):
             s += f' ({hand.index(c)}): ' + c + '  '
         return s
 
+    def play_to_str(pl):
+        s = ''
+        for c in pl:
+            s += f' {c} '
+        return s
+
     def collections_to_str(collections):
         coll_str = ''
         for collection in collections:
             coll_str = coll_str + '\n['
             for play in collection:
-                coll_str = coll_str + '(' + hand_to_str(play) + ')'
+                coll_str = coll_str + '(' + play_to_str(play) + ')'
             coll_str = coll_str + ']'
         return coll_str
     # print played
@@ -131,38 +128,69 @@ def ai_think():
     #     return str(card[0])
 
 
-def discard(cards):
-    return cards.pop()
-
-
-def draw_input(player_number):
-    '''Replaces inline draw code
-    '''
-    pass
+def discard(hand, hi=0):
+    discard_pile.append(hand.pop(hi))
 
 
 def player_turn(player_number):
     global winner
-    global discard_pile
+
+    def draw_phase():
+        global discard_pile
+        draw_source=input('Draw a (N)ew card or from (D)iscard?').lower()[0]
+        if draw_source == 'n':
+            hands[player_number].append(draw())
+            print('You drew a', hands[player_number][-1])
+        elif draw_source == 'd':
+            bottom=int(input("what's the bottom card?"))
+            print(bottom)
+            # needs all cases covered
+            if bottom == 0:
+                hands[player_number].extend(discard_pile)
+                discard_pile = []
+            elif bottom < len(discard_pile)-1:
+                hands[player_number].extend(discard_pile[bottom:])
+                discard_pile = discard_pile[:bottom]
+            elif bottom == len(discard_pile)-1:
+                hands[player_number].append(discard_pile.pop())
+
+    def play_to_collection(play_index_list, player_number):
+        '''REFACTOR
+        '''
+        the_play = []
+        for p in play_index_list:
+            the_play.append(hands[player_number][int(p)])
+        for c in the_play:
+            hands[player_number].remove(c)
+        print(the_play)
+        print(hands[player_number])
+        return the_play
+
+    def discard_phase(player_number):
+        index_to_discard = int(input('what would you like to discard? ()#):'))
+        discard_pile.append(hands[player_number].pop(index_to_discard))
+
     print(f"**** Player {player_number}'s turn: ****")
 
     print_game_for_player(player_number)
-    draw_source = input('Draw a (N)ew card or from (D)iscard?').lower()[0]
-    if draw_source == 'n':
-        hands[player_number].append(draw())
-        print('You drew a', hands[player_number][-1])
-    elif draw_source == 'd':
-        bottom = int(input("what's the bottom card?"))
-        print(bottom)
-        # needs all cases covered
-        if bottom == 0:
-            hands[player_number].extend(discard_pile)
-            discard_pile = []
-        elif bottom < len(discard_pile)-1:
-            hands[player_number].extend(discard_pile[bottom:])
-            discard_pile = discard_pile[:bottom]
+    #TODO catch errors, draw again
+    draw_phase()
 
-    discard_pile.append(discard(hands[player_number]))
+    print_game_for_player(player_number)
+
+    # TODO play_phase function
+    # TODO loop play_phase until NO
+    to_play = 'n'
+    to_play = input('Would you like to (P)la(Y) any of your cards?(Y,N)').lower()[0]
+    if to_play == 'y':
+        what_to_play_str = input('Enter cards you would like to play( \'1,4,12\' ):')
+        what_to_play = what_to_play_str.split(',')
+        collections[player_number].append(play_to_collection(what_to_play, player_number))
+
+    print_game_for_player(player_number)
+    discard_phase(player_number)
+
+    # discard_pile.append(discard(hands[player_number]))
     if len(hands[player_number]) == 0:
         winner = True
         print(f'Player {player_number} wins!')
